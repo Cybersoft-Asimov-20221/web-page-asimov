d8007b2f830e71d2851537c431c8fddef449ae12
Yordy-code81 <U201923959@upc.edu.pe>
2022-05-06 18:26:31 -0500

    fix: app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
