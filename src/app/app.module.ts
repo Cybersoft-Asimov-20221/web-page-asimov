46ecfd8312910c563ebf093dd076ee7777f93330
RickCBs <erikc.cortezb@gmail.com>
2022-05-06 01:27:39 -0500

    feat: competences-part-complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
-import {AppRoutingModule} from "./modules/app-routing.module";
-import {HttpClientModule} from "@angular/common/http";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { AppRoutingModule } from "./modules/app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { CompetencesComponent } from "./competences/pages/competences/competences.component";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetencesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +27,8 @@ import {HttpClientModule} from "@angular/common/http";
     MatSidenavModule,
     MatListModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompetencesComponent } from "../competences/pages/competences/competences.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: 'competences', component: CompetencesComponent},
 ];
 
 @NgModule({
