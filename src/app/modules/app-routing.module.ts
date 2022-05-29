b5cc5fde2c8fdd79a65438b76972af7ea9a9cc5f
“John <leonsahir7@gmail.com>
2022-05-29 15:52:49 -0500

    ~fix~

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AnnouncementTeacherComponent } from "../announcement/pages/announcement-teacher/announcement-teacher.component";
 import { AnnouncementComponent } from '../announcement/pages/announcement/announcement.component';
 import { CompetencesComponent } from "../competences/pages/competences/competences.component";
+import { CourseDetailComponent } from "../courses/pages/course-detail/course-detail.component";
 import { CoursesComponent } from "../courses/pages/courses/courses.component";
 import { DashboardComponent } from "../dashboard/pages/dashboard/dashboard.component";
+import { ProfileComponent } from "../profile/pages/profile/profile.component";
+import { TeacherDetailComponent } from "../teachers/pages/teacher-detail/teacher-detail.component";
 import { TeachersComponent } from "../teachers/pages/teachers/teachers.component";
 import { TopTeachersComponent } from '../top-teachers/pages/top-teachers/top-teachers.component';
-import {CourseDetailComponent} from "../courses/pages/course-detail/course-detail.component";
-import {TeacherDetailComponent} from "../teachers/pages/teacher-detail/teacher-detail.component";
-import {ProfileComponent} from "../profile/pages/profile/profile.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent},
   { path: 'announcements', component: AnnouncementComponent},
   { path: 'announcementsTeacher', component: AnnouncementTeacherComponent},
